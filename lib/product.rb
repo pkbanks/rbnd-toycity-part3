bcc7a8692efb55d6b6cadd7cbffe8e448c8a694a
PK Banks <pkbanks@gmail.com>
2016-05-23 15:39:38 -0400

    added class methods find_by_title and in_stock

--- lib/product.rb
+++ lib/product.rb
@@ -1,17 +1,45 @@
 # lib/product.rb
 
 class Product
-	attr_reader :title
+	attr_reader :title, :price, :stock
 
 	@@products = []
 
 	def initialize(options={})
 		@title = options[:title]
+		@price = options[:price]
+		@stock = options[:stock]
+
 		add_to_products
+
+	end
+
+	def in_stock?
+		
+		# if product's stock is greater than zero
+		# return true
+		# else return false
+
+		@stock > 0 ? true : false
 	end
 
 	def self.all
 		@@products
+	end
+
+	def self.find_by_title(title)
+		@@products.each do |product|
+			return product if product.title == title
+		end
+	end
+
+	def self.in_stock
+		# returns an array of all products with a stock greater than zero
+		result = []
+		@@products.each do |product|
+			result << product if product.in_stock
+		end
+		return result
 	end
 
 	private
