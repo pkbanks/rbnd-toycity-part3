c00276189c1f061d6d15d7ca6fbca6516cc38542
PK Banks <pkbanks@gmail.com>
2016-05-23 15:24:03 -0400

    added duplicate error to handle when a new product name already exists, and made adding a new product into a private method, will need to fire that only if there is no duplicate error

--- lib/product.rb
+++ lib/product.rb
@@ -1,15 +1,22 @@
 # lib/product.rb
 
 class Product
+	attr_reader :title
 
 	@@products = []
 
 	def initialize(options={})
-		@@products << self
+		@title = options[:title]
+		add_to_products
 	end
 
 	def self.all
 		@@products
 	end
 
+	private
+
+	def add_to_products
+		@@products << self
+	end
 end
